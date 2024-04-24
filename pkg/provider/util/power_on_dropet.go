15b48ebad281f0772709bec6b7200be020b83b04
Zoran Zorica <zoran.zorica@gmail.com>
2024-04-24 21:09:44 +0200

    feat: add start/stop droplet (#6)
    Signed-off-by: Zoran Zorica <zoran.zorica@gmail.com>

---
+++ pkg/provider/util/power_on_dropet.go
@@ -0,0 +1,32 @@
+package util
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/digitalocean/godo"
+)
+
+func PowerOnDroplet(client *godo.Client, dropletID int) error {
+	_, _, err := client.DropletActions.PowerOn(context.Background(), dropletID)
+
+	if err != nil {
+		return fmt.Errorf("error powering off droplet: %v", err)
+	}
+
+	for {
+		droplet, _, err := client.Droplets.Get(context.Background(), dropletID)
+		if err != nil {
+			return fmt.Errorf("error getting a droplet: %v", err)
+		}
+
+		if droplet.Status == "active" {
+			break
+		}
+
+		time.Sleep(time.Second * 10)
+	}
+
+	return nil
+}
