2189dac2231821fa6161f7e2d8653eb6fff41e9e
Toma Puljak <toma.puljak@hotmail.com>
2024-08-13 06:51:32 +0000

    fix: detach volume before deleting
    Signed-off-by: Toma Puljak <toma.puljak@hotmail.com>

--- pkg/provider/stop.go
+++ pkg/provider/stop.go
@@ -1,10 +1,7 @@
 package provider
 
 import (
-	"context"
 	"io"
-	"strings"
-	"time"
 
 	log_writers "github.com/daytonaio/daytona-provider-digitalocean/internal/log"
 	"github.com/daytonaio/daytona-provider-digitalocean/pkg/provider/util"
@@ -36,30 +33,10 @@ func (p *DigitalOceanProvider) StopWorkspace(workspaceReq *provider.WorkspaceReq
 		return nil, err
 	}
 
-	droplet, err := util.GetDroplet(client, util.GetDropletName(workspaceReq.Workspace))
-	if err != nil {
-		logWriter.Write([]byte("Failed to get droplet ID: " + err.Error() + "\n"))
-		return nil, err
-	}
-
-	err = util.DeleteDroplet(client, droplet.ID)
+	err = util.DeleteDroplet(client, workspaceReq.Workspace, false)
 	if err != nil {
 		logWriter.Write([]byte("Failed to delete droplet: " + err.Error() + "\n"))
 		return nil, err
-	}
-
-	for {
-		_, _, err := client.Droplets.Get(context.Background(), droplet.ID)
-		if err != nil {
-			if strings.Contains(err.Error(), "404") {
-				break
-			} else {
-				logWriter.Write([]byte("Failed to get droplet: " + err.Error() + "\n"))
-				return nil, err
-			}
-		}
-
-		time.Sleep(1 * time.Second)
 	}
 
 	logWriter.Write([]byte("Droplet deleted.\n"))
--- pkg/provider/util/delete_droplet.go
+++ pkg/provider/util/delete_droplet.go
@@ -2,22 +2,77 @@ package util
 
 import (
 	"context"
+	"strings"
+	"time"
 
+	"github.com/daytonaio/daytona/pkg/workspace"
 	"github.com/digitalocean/godo"
 )
 
-func DeleteDroplet(client *godo.Client, dropletID int) error {
-	_, err := client.Droplets.Delete(context.Background(), dropletID)
-	return err
-}
+func DeleteDroplet(client *godo.Client, workspace *workspace.Workspace, deleteVolume bool) error {
+	if deleteVolume {
+		err := DeleteVolume(client, GetDropletName(workspace))
+		if err != nil {
+			return err
+		}
+	}
 
-func DeleteVolume(client *godo.Client, volumeID string) error {
-	volume, err := GetVolumeByName(client, volumeID)
+	droplet, err := GetDroplet(client, GetDropletName(workspace))
 	if err != nil {
 		return err
 	}
-	volumeID = volume.ID
 
-	_, err = client.Storage.DeleteVolume(context.Background(), volumeID)
+	_, err = client.Droplets.Delete(context.Background(), droplet.ID)
+	if err != nil {
+		return err
+	}
+
+	for {
+		_, _, err := client.Droplets.Get(context.Background(), droplet.ID)
+		if err != nil {
+			if strings.Contains(err.Error(), "404") {
+				break
+			} else {
+				return err
+			}
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+
+	return nil
+}
+
+func DeleteVolume(client *godo.Client, name string) error {
+	volume, err := GetVolumeByName(client, name)
+	if err != nil {
+		return err
+	}
+
+	if volume == nil {
+		return nil
+	}
+
+	ctx := context.Background()
+
+	for _, dropletID := range volume.DropletIDs {
+		_, _, err = client.StorageActions.DetachByDropletID(ctx, volume.ID, dropletID)
+		if err != nil {
+			return err
+		}
+	}
+
+	for len(volume.DropletIDs) > 0 {
+		time.Sleep(time.Second)
+
+		volume, err = GetVolumeByName(client, name)
+		if err != nil {
+			return err
+		} else if volume == nil {
+			break
+		}
+	}
+
+	_, err = client.Storage.DeleteVolume(context.Background(), volume.ID)
 	return err
 }
